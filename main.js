d7c0575f42c3cdb14b8544a18c679968b0096de3
josealeperdomo <perdomomolinajose@gmail.com>
2023-10-27 20:52:02 -0400

    ..

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-/* Ejercicios para practicar */
-/*
-1) Crea una función para obtener el factorial de un numero.
-2) Crea una función que tome un array y un elemento, que añada el elemento al final del array.
-3) Crea una función que tome un array y elimine el primer elemento.
-4) Crea una función que tome un array de números  y devuelva un nuevo array que contenga solo los números impares.
-5) Crea una función que tome un array y un elemento, y elimine todas las ocurrencias de ese elemento en el array.
-6) Dada la siguiente frase = "JavaScript es un lenguaje de programación versátil y poderoso.";
-   * Contar el numero de palabras en la frase. 
-   * Reemplazar una palabra especifica de la frase.
-   * Convertir la frase en un array de palabras y ordenarlas alfabéticamente.
-   * Convertir toda la frase en Mayúsculas.
-*/
-
 //1
 
 function factorial(num){
@@ -92,3 +78,62 @@ console.log(arraytexto.sort());
 //------------------------------------------------------
 console.log(texto.toUpperCase());
 //------------------------------------------------------
+
+//EJERCICIO 27/10
+/*
+    Crear una clase llamada Personaje, que tenga los siguientes atributos:
+    nombre, apodo, edad, salud, fuerza, rareza, y que tenga los siguientes metodos:
+    atacar, defender, saludar (Estos metodos deben imprimir en consola, solo eso (get)).
+    Debes crear 5 personajes con diferentes atributos y ejecutar los metodos de cada uno.
+*/
+
+class Personaje{
+    constructor(nombre,apodo,edad,salud,fuerza,rareza){
+        this._nombre = nombre
+        this._apodo = apodo
+        this._edad = edad
+        this._salud = salud
+        this._fuerza = fuerza
+        this._rareza = rareza
+    }
+    get atacar(){
+        return console.log(`${this._nombre} está atacando`);
+    }
+    get defender(){
+        return console.log(`${this._nombre} se está defendiendo`);
+    }
+    get saludar(){
+        console.log(`Hola, soy ${this._nombre}`);
+    }
+}
+
+let personaje1 = new Personaje('PacMan', 'Comefantasmas', 20, 100, 5, 'Especial')
+let personaje2 = new Personaje('Sonic', 'Erizo', 17, 100, 8, 'Raro')
+let personaje3 = new Personaje('Kratos', 'Dios de la Guerra', 200, 100, 10, 'Legendario')
+let personaje4 = new Personaje('Kang', 'El Conquistador', 40, 100, 10, 'Epico')
+let personaje5 = new Personaje('Galactus', 'El Devorador de mundos', 500, 100, 10, 'Legendario')
+
+console.log(personaje1);
+personaje1.atacar
+personaje1.defender
+personaje1.saludar
+
+console.log(personaje2);
+personaje2.atacar
+personaje2.defender
+personaje2.saludar
+
+console.log(personaje3);
+personaje3.atacar
+personaje3.defender
+personaje3.saludar
+
+console.log(personaje4);
+personaje4.atacar
+personaje4.defender
+personaje4.saludar
+
+console.log(personaje5);
+personaje5.atacar
+personaje5.defender
+personaje5.saludar
